26851df2b93ad3abd83c8554d5ca0047b5887551
HARISH U <harishinkx@gmail.com>
2026-02-15 22:10:54 +0530

    added comments to main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
 from imblearn.over_sampling import SMOTE
 import joblib
-# 1. Load dataset
+#Load dataset
 data = pd.read_csv("data/cs-training.csv")
 # Behavioral engineered signals
 data["balance_drop_signal"] = np.random.randint(0,2,len(data))
@@ -25,45 +25,45 @@ data.rename(columns={
 }, inplace=True)
 
 
-# Drop unnecessary column
+#Drop unnecessary column
 data = data.drop(columns=["Unnamed: 0"])
 
-# 2. Fill missing values
+#Fill missing values
 data.fillna(data.median(), inplace=True)
 
-# 3. Features & target
+#Features & target
 X = data.drop("SeriousDlqin2yrs", axis=1)
 y = data["SeriousDlqin2yrs"]
 
-# 4. Train test split
+#Train test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Apply SMOTE
+#Apply SMOTE
 smote = SMOTE(random_state=42)
 X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
 
-# Train model
+#Train model
 model = XGBClassifier()
 model.fit(X_train_sm, y_train_sm)
 
 joblib.dump(model, "model.pkl")
 joblib.dump(X.columns.tolist(), "features.pkl")
 
-# 6. Predictions
+#Predictions
 preds = model.predict(X_test)
 
 print(classification_report(y_test, preds))
 
-# 7. Example customer prediction
+#Example customer prediction
 sample = X_test.iloc[0].values.reshape(1, -1)
 
 risk_prob = model.predict_proba(sample)[0][1]
 
 print("\nCustomer Risk Score:", round(risk_prob*100,2), "%")
 
-# 8. Intervention logic
+#Intervention logic
 if risk_prob > 0.7:
     print("Recommended Action: Offer payment holiday / restructure")
 elif risk_prob > 0.4:
